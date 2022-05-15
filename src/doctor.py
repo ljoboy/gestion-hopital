341311edf6bc9003da0222266acc2771f799e453
Dark Angel <jonathanyombo@gmail.com>
2022-05-16 01:17:14 +0200

    specialization

--- src/doctor.py
+++ src/doctor.py
@@ -5,18 +5,37 @@ class Doctor:
     """
     Class representing a doctor
     constructor with some params
-        nom: str,
-        prenom: str,
-        postnom: str,
-        phone: str,
+        nom: str
+        prenom: str
+        postnom: str
+        phone: str
+        ---------
+    others properties
+        specialization: [str]
     """
-    _matricule: str
 
-    def __init__(self, nom: str, prenom: str, postnom: str, phone: str):
+    def __init__(self, nom: str, prenom: str, postnom: str, phone: str) -> None:
         if not str.isalnum(nom) or not str.isalnum(prenom) or not str.isalnum(postnom) or not str.isalnum(phone):
             raise TypeError
         self._nom = nom
         self._prenom = prenom
         self._postnom = postnom
         self._phone = phone
+        self._specialization = []
 
+    @property
+    def specialization(self) -> list:
+        return self._specialization
+
+    @specialization.setter
+    def specialization(self, value: [str]) -> None:
+        if isinstance(value, list):
+            for val in value:
+                if not isinstance(val, str):
+                    raise TypeError
+                elif val.strip():
+                    self._specialization.append(val.strip().lower())
+        elif isinstance(value, str):
+            self._specialization.append(value.lower())
+        else:
+            raise TypeError
--- tests/test_doctor.py
+++ tests/test_doctor.py
@@ -2,12 +2,16 @@ import pytest
 from src.doctor import Doctor
 
 
+@pytest.fixture()
+def doctor():
+    return Doctor("nom", "prenom", "postnom", "phone")
+
+
 class TestDoctor:
 
-    def test_case01(self):
+    def test_case01(self, doctor):
         """ Test if it's record all Doctor infos """
-        d1 = Doctor("nom", "prenom", "postnom", "phone")
-        assert isinstance(d1, Doctor)
+        assert isinstance(doctor, Doctor)
 
     def test_case02_01(self):
         """ Test if it's raise an Exception (TypeError) when instantiate without params """
@@ -49,6 +53,37 @@ class TestDoctor:
         """ Test if it's raise an Exception with bad params """
         with pytest.raises(TypeError):
             d1 = Doctor("nom", "prenom", "postnom", ['telephone'])
+
+    def test_case04(self, doctor):
+        """ Test if we can get specialization and it's an empty array """
+        assert doctor.specialization == []
+
+    def test_case05_01(self, doctor):
+        """ Test if we can assign specialization with list and str to doctor """
+        specs = ['Gynecology', "pediatry".capitalize()]
+        spec = 'Traumatologie'
+        doctor.specialization = specs
+        doctor.specialization = spec
+        specs = [specs[0].lower(), specs[1].lower(), spec.lower()]
+        assert doctor.specialization == specs
+
+    def test_case05_02(self, doctor):
+        """ Test if int with raise TypeError """
+        with pytest.raises(TypeError):
+            doctor.specialization = 55
+
+    def test_case05_03(self, doctor):
+        """ Test if an array with a bad type will raise TypeError """
+        with pytest.raises(TypeError):
+            doctor.specialization = ["Radiology", True]
+
+    def test_case05_04(self, doctor):
+        """ Ensure that all upper words will be returned as lower """
+        spec = "DENTISTE"
+        doctor.specialization = spec
+        assert [spec.lower()] == doctor.specialization
+
+
 
 
 if __name__ == '__main__':
