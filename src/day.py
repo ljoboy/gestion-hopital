2a9c0c277be4799de4eeb496b12587970e374805
Dark Angel <jonathanyombo@gmail.com>
2022-05-30 03:49:04 +0200

    create Day enum

---
+++ src/day.py
@@ -0,0 +1,21 @@
+from enum import Enum
+
+
+class Day(Enum):
+    """
+    Enum representant les jours de la semaine
+    LUNDI = 0
+    MARDI = 1
+    MERCREDI = 2
+    JEUDI = 3
+    VENDREDI = 4
+    SAMEDI = 5
+    DIMANCHE = 6
+    """
+    LUNDI = 0
+    MARDI = 1
+    MERCREDI = 2
+    JEUDI = 3
+    VENDREDI = 4
+    SAMEDI = 5
+    DIMANCHE = 6
